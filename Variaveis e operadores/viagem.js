88220f6d372c203b3a45ef70f5ad9919ca14812f
Fabiana Kraft <fabianadspk@gmail.com>
2023-07-13 18:43:58 -0300

    Update viagem.js

--- Variaveis e operadores/viagem.js
+++ Variaveis e operadores/viagem.js
@@ -1,1 +1,25 @@
-console.log("oi")
+var precoCombustivel=0
+var gastoMedio=0
+var distancia=0
+const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Preço do combustível: ", function(precoCombustivel) { //$/L
+     precoCombustivel = precoCombustivel
+    rl.question("Gasto medio de combustivel: ", function(gastoMedio) { //L/km
+             gastoMedio = gastoMedio
+            rl.question("Km rodados: ", function(distancia){ //km
+                 distancia = distancia
+                 var precoTotal = precoCombustivel*gastoMedio*distancia
+                console.log(`Você gastou na sua viagem: ${precoTotal} reais`)
+                rl.close();
+            })
+    })
+})
+
+rl.on("close", function() {
+    process.exit(0)
+});
